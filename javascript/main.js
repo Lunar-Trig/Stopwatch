145dd7d8289eeb0da7befcdb76a2a3a2f30c9400
Stacey Nkongchu <91387650+Lunar-Trig@users.noreply.github.com>
2022-05-09 19:45:15 -0500

    finished html and css, little bit of js

---
+++ javascript/main.js
@@ -0,0 +1,4 @@
+const span = document.querySelector('span');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const resetBtn = document.getElementById('reset-btn');
